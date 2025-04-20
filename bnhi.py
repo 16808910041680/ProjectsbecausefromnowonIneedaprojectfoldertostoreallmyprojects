e78477caa337d05af6c85fafa30a07e124eb4c37
16808910041680 <forforfirefire@gmail.com>
2025-04-20 20:59:41 +1000

    k

---
+++ bnhi.py
@@ -0,0 +1,8 @@
+a = int ((input ("Enter Your Birthday's Day")))
+b = int ((input ("Enter Your Birthday's Month")))
+c = int (input ("Enter Your Birthday's Year"))
+print ("You were born on the", a, "of", b, "in", c)
+num1 = 2025 - c
+num2 = 3 - b
+num3 = 23 - a
+print ("That makes you", num1, "years,", num2, "months, and", num3, "days old.")
